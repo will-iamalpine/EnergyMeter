2d17309ff58f3f8edbdaea301ef318b04a708b6c
Will Buchanan <will@buchananwp.com>
2019-03-01 15:01:22 -0800

    small tweaks

--- data_clean.py
+++ data_clean.py
@@ -3,19 +3,15 @@ import glob
 import numpy as np
 import pandas as pd
 
-data_path = '/Users/willbuchanan/Google Drive/ricky-will-louis/data/training/'
-var_names = {'power_factor': 'Power factor', 'phase_angle': 'Phase angle', 'power_real': 'Real power',
- 'power_reactive': 'Reactive power', 'power_apparent': 'Apparent power', 'vrms': 'RMS voltage', 'irms': 'RMS current'}
-
-os.chdir('/Users/willbuchanan/Google Drive/ricky-will-louis/data/training/')
+data_path = "/Users/willbuchanan/Google Drive/GIX/ricky-will-louis/data/training/"
 
 vars = ['time','power_factor','phase_angle','power_real', 'power_reactive', 'power_apparent', 'vrms', 'irms']
 
-data_path = '/Users/willbuchanan/Google Drive/ricky-will-louis/data/training/'
 os.chdir(data_path)
 
 train_labels = []
-train_data = []
+data_train = []
+
 for appliance_type in os.listdir("."):
     if appliance_type.endswith('.csv'):
         #split_file = str(filename).split("_")
@@ -34,3 +30,6 @@ for appliance_type in os.listdir("."):
         train_labels.append(str(app_df['app_name'][0]).split('-')[0])
 
 data_train_np = np.array(data_train)
+
+print('array created with shape:',data_train_np.shape)
+print('unique labels:',set(train_labels))

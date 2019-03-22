603389bfc08833874bc763697157a806531e00b1
quicksell-louis <quicksell.louis@gmail.com>
2019-03-22 12:53:59 -0700

    Edited visualization to plot signature data for each appliance

--- scripts/app.py
+++ scripts/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 import plotly.graph_objs as go
 
-data_path = '/Users/louis/Google Drive/ricky-will-louis/data/training/kettle_ricky_7.csv'
+data_path = '/Users/louis/Google Drive/ricky-will-louis/data/data_train/'
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 var_names = {'power_factor': 'Power factor', 'phase_angle': 'Phase angle', 'power_real': 'Real power',
  'power_reactive': 'Reactive power', 'power_apparent': 'Apparent power', 'vrms': 'RMS voltage', 'irms': 'RMS current'}
@@ -14,108 +14,126 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app_df = pd.read_csv(data_path)
-app_df.columns = app_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+vars = ['time', 'power_factor', 'phase_angle', 'power_real', 'power_reactive', 'power_apparent', 'vrms', 'irms']
+
+cell_df = pd.read_csv(data_path+'cell_1.csv', names = vars)
+cell_df.columns = cell_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+lamp_df = pd.read_csv(data_path+'desklamp_1.csv', names = vars)
+lamp_df.columns = lamp_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+fan_df = pd.read_csv(data_path+'fan_1.csv', names = vars)
+fan_df.columns = fan_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+kettle_df = pd.read_csv(data_path+'kettle_1.csv', names = vars)
+kettle_df.columns = kettle_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+laptop_df = pd.read_csv(data_path+'laptop_1.csv', names = vars)
+laptop_df.columns = laptop_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+monitor_df = pd.read_csv(data_path+'monitor_1.csv', names = vars)
+monitor_df.columns = monitor_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+sadlamp_df = pd.read_csv(data_path+'sadlamp_1.csv', names = vars)
+sadlamp_df.columns = sadlamp_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
 
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-trace1 = go.Scatter(
-    x=app_df['time'],
-    y=app_df['power_factor'],
-    text=[str('Power factor: ' + str(data)) for data in app_df['power_factor']],
-    hoverinfo='text',
-    mode='lines',
-    opacity=0.7,
-    line=dict(
-            color = 'rgb(255,127,0)',
-            width = 2,
-    ),
-    name='Power factor'
-)
-trace2 = go.Scatter(
-    x=app_df['time'],
-    y=app_df['phase_angle'],
-    text=[str('Phase angle: ' + str(data) + ' ø') for data in app_df['phase_angle']],
-    hoverinfo='text',
-    mode='lines',
-    opacity=0.7,
-    line=dict(
-            color = 'rgb(227,26,28)',
-            width = 2,
-    ),
-    name='Phase angle',
-    yaxis='y2'
-)
+df_list = [cell_df, lamp_df, fan_df, kettle_df, laptop_df, monitor_df, sadlamp_df]
+df_names = ['Cell', 'Lamp', 'Fan', 'Kettle', 'Laptop', 'Monitor', 'Sad lamp']
+pf_list = []
+phase_list = []
+real_list = []
+reac_list = []
+appar_list = []
+vrms_list = []
+irms_list = []
+for i,df in enumerate(df_list):
+    pf_list.append(go.Scatter(
+                x=df['time'],
+                y=df['power_factor'],
+                text=[str(str(df_names[i]) + ' Power factor: ' + str(data)) for data in df['power_factor']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Power factor')))
 
-real_p = go.Scatter(
-    x=app_df['time'],
-    y=app_df['power_real'],
-    text=[str('Real power: ' + str(data) + ' W') for data in app_df['power_real']],
-    hoverinfo='text',
-    mode='lines',
-    opacity=0.7,
-    line=dict(
-            color = 'rgb(136,65,157)',
-            width = 2,
-    ),
-    name='Real power'
-)
+    phase_list.append(go.Scatter(
+                x=df['time'],
+                y=df['phase_angle'],
+                text=[str(str(df_names[i]) + ' Phase angle: ' + str(data)) for data in df['phase_angle']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Phase angle')))
 
-app_p = go.Scatter(
-    x=app_df['time'],
-    y=app_df['power_apparent'],
-    text=[str('Apparent power: ' + str(data) + ' W') for data in app_df['power_apparent']],
-    hoverinfo='text',
-    mode='lines',
-    opacity=0.7,
-    line=dict(
-            color = 'rgb(43,140,190)',
-            width = 2,
-    ),
-    name='Apparent power'
-)
+    real_list.append(go.Scatter(
+                x=df['time'],
+                y=df['power_real'],
+                text=[str(str(df_names[i]) + ' Real power: ' + str(data)) for data in df['power_real']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Real power')))
 
-reac_p = go.Scatter(
-    x=app_df['time'],
-    y=app_df['power_reactive'],
-    text=[str('Reactive power: ' + str(data) + ' W') for data in app_df['power_reactive']],
-    hoverinfo='text',
-    mode='lines',
-    opacity=0.7,
-    line=dict(
-            color = 'rgb(215,48,31)',
-            width = 2,
-    ),
-    name='Reactive power'
-)
+    reac_list.append(go.Scatter(
+                x=df['time'],
+                y=df['power_reactive'],
+                text=[str(str(df_names[i]) + ' Reactive power: ' + str(data)) for data in df['power_reactive']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Reactive power')))
+
+    appar_list.append(go.Scatter(
+                x=df['time'],
+                y=df['power_apparent'],
+                text=[str(str(df_names[i]) + ' Apparent power: ' + str(data)) for data in df['power_apparent']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Apparent power')))
+
+    vrms_list.append(go.Scatter(
+                x=df['time'],
+                y=df['vrms'],
+                text=[str(str(df_names[i]) + ' Voltage: ' + str(data)) for data in df['vrms']],
+                hoverinfo='text',
+                mode='lines',
+                opacity=0.7,
+                line=dict(
+                        width = 2,),name=str(str(df_names[i]) + ' Voltage')))
+
+    irms_list.append(go.Scatter(
+        x=df['time'],
+        y=df['irms'],
+        text=[str(str(df_names[i]) + ' Current: ' + str(data)) for data in df['irms']],
+        hoverinfo='text',
+        mode='lines',
+        opacity=0.7,
+        line=dict(
+            width=2, ), name=str(str(df_names[i]) + ' Current')))
+
 
 app.layout = html.Div([
     html.H1(children='EnergyMeter'),
-    html.Div('You are now viewing data for:'),
-    html.Div('Appliance: ' + app_df['app_name'][1]),
-    html.Div('Description: ' + app_df['app_desc'][1]),
-    html.Div('Sample id: ' + str(app_df['sample_id'][1])),
+    html.Div('You are now viewing the energy signatures of a sample of devices.'),
 
     dcc.Graph(
           id='voltage',
           figure={
-            'data':[
-                go.Scatter(
-                    x=app_df['time'],
-                    y=app_df['vrms'],
-                    text=[str('Voltage: ' + str(data) + ' V') for data in app_df['vrms']],
-                    hoverinfo='text',
-                    mode='lines',
-                    line=dict(
-                        color = 'rgb(31,120,180)',
-                        width = 2
-                ))
-            ],
+            'data':vrms_list,
             'layout': go.Layout(
                 title='Voltage (V)',
-                xaxis={'title': 'Time (s)'},
+                xaxis=dict(title='Time (s)',
+                           range=[0,7.2]),
                 yaxis=dict(
                         title='RMS Voltage (V)'
                         ),
@@ -127,21 +145,11 @@ app.layout = html.Div([
     dcc.Graph(
           id='current',
           figure={
-            'data':[
-                go.Scatter(
-                    x=app_df['time'],
-                    y=app_df['irms'],
-                    text=[str('Current: ' + str(data) + ' A') for data in app_df['irms']],
-                    hoverinfo='text',
-                    mode='lines',
-                    line=dict(
-                        color = 'rgb(178,223,138)',
-                        width = 2
-                        ))
-                ],
+            'data':irms_list,
             'layout': go.Layout(
                 title='Current (A)',
-                xaxis={'title': 'Time (s)'},
+                xaxis=dict(title='Time (s)',
+                           range=[0,7.2]),
                 yaxis=dict(
                     title='RMS Current (Amp)'
                 ),
@@ -149,47 +157,69 @@ app.layout = html.Div([
             )
           }),
 
+
     dcc.Graph(
-        id='phase_pf',
-        figure={
-        'data':[trace1, trace2],
-        'layout': go.Layout(
-            title='Power factor and Phase angle (ø)',
-            yaxis2=dict(
+          id='phase',
+          figure={
+            'data':phase_list,
+            'layout': go.Layout(
                 title='Phase angle (ø)',
-                titlefont=dict(
-                    color='rgb(227,26,28)'
+                xaxis=dict(title='Time (s)',
+                           range=[0,7.2]),
+                yaxis=dict(
+                    title='Phase angle (ø)'
                 ),
-                tickfont=dict(
-                    color='rgb(227,26,28)'
-                ),
-                overlaying='y',
-                side='right'
-            ),
-            yaxis=dict(
+                hovermode='closest'
+            )
+          }),
+
+    dcc.Graph(
+          id='power factor',
+          figure={
+            'data':pf_list,
+            'layout': go.Layout(
                 title='Power factor',
-                titlefont=dict(
-                    color='rgb(255,127,0)'
+                xaxis=dict(title='Time (s)',
+                           range=[0,7.2]),
+                yaxis=dict(
+                    title='Power facotr'
                 ),
-                tickfont=dict(
-                    color='rgb(255,127,03)'
-                ),
+                hovermode='closest'
             )
-            )
+          }),
+
+    dcc.Graph(
+        id='real power',
+        figure={
+            'data': real_list,
+            'layout': go.Layout(
+                title="Real Power (W)",
+                xaxis=dict(range=[0,7.2]),
+                yaxis=dict(
+                       title='Real Power (W)'))
         }),
 
+    dcc.Graph(
+        id='reactive power',
+        figure={
+            'data': reac_list,
+            'layout': go.Layout(
+                title="Reactive Power (W)",
+                xaxis=dict(range=[0,7.2]),
+                yaxis=dict(
+                       title='Reactive Power (W)'))
+        }),
 
     dcc.Graph(
-        id='powers',
+        id='apparent power',
         figure={
-            'data': [reac_p, real_p, app_p],
+            'data': appar_list,
             'layout': go.Layout(
-                title="Real, Apparent, and Reactive Power (W)",
+                title="Apparent Power (W)",
+                xaxis=dict(range=[0,7.2]),
                 yaxis=dict(
-                       title='Power (W)'))
-        }
-    )
-
+                       title='Apparent Power (W)'))
+        }),
 ])
 
 app.css.append_css({
